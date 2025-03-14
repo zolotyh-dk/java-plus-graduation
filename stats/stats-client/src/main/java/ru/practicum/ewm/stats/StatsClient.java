a87feb2fc3cdf96401e091aa54591defc227c97a
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2025-03-14 22:58:11 +0300

    Feat: Add gateway-server and config-server

--- stats/stats-client/src/main/java/ru/practicum/ewm/stats/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/ewm/stats/StatsClient.java
@@ -1,10 +1,23 @@
 package ru.practicum.ewm.stats;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@FeignClient(name = "statsClient")
 public interface StatsClient {
-    void saveHit(EndpointHitDto hitDto);
+    @PostMapping( "/hit")
+    void saveHit(@RequestBody EndpointHitDto hitDto);
 
-    List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique);
+    @GetMapping( "/stats")
+    List<ViewStatsDto> getStats(@RequestParam("start") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+                                @RequestParam("end") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+                                @RequestParam("uris") List<String> uris,
+                                @RequestParam("unique") boolean unique);
 }
