e4214fe6c5c755432ee22dc6fa9a7383331ad3ec
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2025-04-05 22:11:09 +0300

    Feat: Add base for aggregator service

---
+++ stats/aggregator/src/main/java/ru/practicum/ewm/AggregatorApp.java
@@ -0,0 +1,13 @@
+package ru.practicum.ewm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class AggregatorApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AggregatorApp.class, args);
+    }
+}
